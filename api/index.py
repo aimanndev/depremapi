180f17f1475aa405f06476a726053ffd7048b424
aimanndev <possumissm@gmail.com>
2026-04-25 15:01:28 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -11,17 +11,24 @@ def home():
         r = requests.get("http://udim.koeri.boun.edu.tr/zeqmap/xmlt/son24saat.xml", timeout=5)
         root = ET.fromstring(r.content)
         
-        # En son depremi ayıkla
-        last = root.find('earhquake')
+        # Tüm depremleri bir liste olarak al
+        all_quakes = root.findall('earhquake')
         
-        data = {
-            "status": "success",
-            "yer": last.get('lokasyon').strip(),
-            "mag": float(last.get('mag')),
-            "lat": float(last.get('lat')),
-            "lng": float(last.get('lng')),
-            "zaman": last.get('name')
-        }
-        return jsonify(data)
+        if len(all_quakes) > 0:
+            # Reis dediğin gibi liste tersmiş, o yüzden [-1] ile en alttakini alıyoruz
+            last = all_quakes[-1] 
+            
+            data = {
+                "status": "success",
+                "yer": last.get('lokasyon').strip(),
+                "mag": float(last.get('mag')),
+                "lat": float(last.get('lat')),
+                "lng": float(last.get('lng')),
+                "zaman": last.get('name')
+            }
+            return jsonify(data)
+        else:
+            return jsonify({"status": "error", "message": "Deprem bulunamadı"})
+            
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)})
